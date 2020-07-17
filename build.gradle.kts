a372f24077a980a7e1d0d6ee8d90e9451752676f
Arthur Gavlyukovskiy <agavlyukovskiy@gmail.com>
2020-07-18 01:27:41 +0300

    test

--- build.gradle.kts
+++ build.gradle.kts
@@ -5,48 +5,12 @@ buildscript {
 }
 
 plugins {
-    java
     id("pl.allegro.tech.build.axion-release").version("1.12.0")
-}
-
-scmVersion {
-    with(tag) {
-        prefix = ""
-        versionSeparator = ""
-    }
 }
 
 group = "com.github.gavlyukovskiy"
 version = scmVersion.version
 
-java.sourceCompatibility = JavaVersion.VERSION_1_8
-
 repositories {
     mavenCentral()
-}
-
-tasks {
-    val releaseCheck by registering {
-        doLast {
-            val errors = ArrayList<String>()
-            if (System.getenv("GITHUB_ACTIONS") != "true") {
-                if (!project.hasProperty("release.version")) {
-                    errors.add("'-Prelease.version' must be set")
-                }
-                if (!project.hasProperty("release.customUsername")) {
-                    errors.add("'-Prelease.customUsername' must be set")
-                }
-                if (!project.hasProperty("release.customPassword")) {
-                    errors.add("'-Prelease.customPassword' must be set")
-                }
-            }
-            if (errors.isNotEmpty()) {
-                throw IllegalStateException(errors.joinToString("\n"))
-            }
-        }
-    }
-
-    verifyRelease {
-        dependsOn(releaseCheck, build)
-    }
 }
